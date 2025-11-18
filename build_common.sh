ba5bb9c24360d18a8d5a3451772a7ca37fada24a
ruhalis linux <arlan.baikurazov@gmail.com>
2025-11-18 09:57:24 +0000

    fix

--- build_common.sh
+++ build_common.sh
@@ -1,4 +1,4 @@
-cd ~/roarm_ws_em0
+cd ~/roarm_calibration
 colcon build
 colcon build --packages-select roarm_web_app launch_api ros2web_app ros2web_widgets ros2web ros2web_example_py --symlink-install 
-source install/setup.bash 
+source ~/roarm_calibration/install/setup.bash 
